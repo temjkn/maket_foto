155e73435c9fd05dc1a9f83a861a947abee456b0
temjkn <temich89@mail.ru>
2018-10-13 10:53:17 +0300

    maket

---
+++ myJs.js
@@ -0,0 +1,7 @@
+elem.onclick=function(){
+            if(search.classList.contains("search_hid")){
+                search.classList.remove("search_hid");          
+            }else{
+                search.classList.add("search_hid");
+            }
+}
